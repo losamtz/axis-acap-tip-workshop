4bf53bec2dc8e0b461a3604d60fb3ab369b92856
Andrés Martinez-Losa <andres.martinez-losa@axis.com>
2025-11-18 09:54:27 +0100

    remove webserver-proxy/websocket-mdb

--- webserver-proxy/websocket-mdb/app/civet-ws.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "civetweb.h"
-
-int RootHandler(struct mg_connection *conn, void *ud);
-int ws_connect_handler(const struct mg_connection *conn, void *ud);
-void ws_ready_handler(struct mg_connection *conn, void *ud);
-int ws_data_handler(struct mg_connection *conn, int flags, char *data, size_t len, void *ud);
-void ws_close_handler(const struct mg_connection *conn, void *ud);
-void broadcast_text(const char *msg, size_t len);
